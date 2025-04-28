05b5b780393f5af62570c986bb753f2cbbca5978
marksman456 <hank6831511@yahoo.com.tw>
2025-04-28 10:24:38 +0800

    課堂筆記

--- src/BasicDeclare.java
+++ src/BasicDeclare.java
@@ -1,31 +1,35 @@
 public class BasicDeclare {
     public static void main(String[] args) throws Exception {
-        /* 基本型別介紹
-         * int:     整數, 範圍: -2,147,483,648 ~ 2,147,483,647
+        /*
+         * 基本型別介紹
+         * int: 整數, 範圍: -2,147,483,648 ~ 2,147,483,647
          *
-         * byte:    整數, 範圍: -128 ~ 127
+         * byte: 整數, 範圍: -128 ~ 127
          *
-         * long:    整數, 範圍: -9,223,372,036,854,775,808 ~ 9,223,372,036,854,775,807
+         * long: 整數, 範圍: -9,223,372,036,854,775,808 ~ 9,223,372,036,854,775,807 (超過基本數值
+         * 最後加上L)
          *
-         * float:   32位元浮點數, 需要加上F
+         * float: 32位元浮點數, 需要加上F
          *
-         * double:  64位元浮點數, 預設型別
+         * double: 64位元浮點數, 預設型別
          *
-         * char:    字元, 1個字元, 單引號
+         * char: 字元, 1個字元, 單引號
          *
-         * String:  文字, 多個字元, 雙引號
+         * String: 文字, 多個字元, 雙引號
          *
          * boolean: 布林, true/false
          */
         // int age = 25;
         // System.out.println(age);
 
+        // long numbers =1231231231221312345L; (超過基本數值 最後加上L)
+
         // float weight1 = 49.4F;
-        // System.out.println(weight1);
+        // System.out.println(weight1); 需要加上F
 
         // double weight2 = 49.49;
         // System.out.println(weight2);
-        
+
         // char c1 = 'Y';
         // System.out.println(c1);
 
@@ -35,18 +39,25 @@ public class BasicDeclare {
         // boolean isStudent = true; // false
         // System.out.println(isStudent);
 
-
         /*
          * 陣列(Array)宣告
          * 陣列的長度是固定的, 不能改變
          * 陣列的長度是從0開始計算
          */
-        // int[] bingo = {7, 22, 89, 55, 33, 99};
+        // int[] bingo = { 7, 22, 89, 55, 33, 99 };
         // System.out.println(bingo[5]);
         // System.out.println(bingo[2]);
 
-        // String[] students = {"A1", "A2", "A3"};
-        // System.out.println(students[1]);
- String name="123123123132";
+        // boolean[] rainy = { true, false };
+
+        // System.out.println("Is rainy or not:" + rainy[0]);
+        // System.out.println(rainy[1]);
+
+        // String[] students = { "A1", "A2", "A3" };
+        // for (int i = 0; i < students.length; i++) {
+        //     System.out.println(students[i]);
+
+        // }
+        
     }
 }
